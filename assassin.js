833477d6052ff43d5d1ba5606f6228d7407b9001
eli block <eliblock@yahoo.com>
2017-02-12 10:34:10 -0800

    split files

---
+++ assassin.js
@@ -0,0 +1,10 @@
+$(function() {
+  var camera = document.getElementById('camera');
+  var frame = document.getElementById('frame');
+
+  camera.addEventListener('change', function(e) {
+    var file = e.target.files[0]; 
+     //Do something with the image file.
+    frame.src = URL.createObjectURL(file);
+  });
+});
